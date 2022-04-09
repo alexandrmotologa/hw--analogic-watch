a04516e0361043253cec14fd7d590c3e5bff58f8
Alexander Minaku <al.motologa@gmail.com>
2022-04-09 23:42:22 +0300

    up 2.0

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 #main application module
 from clock import *
+from time import sleep
+from datetime import datetime
 
-printTime()
-drawClock()
+while True:
+    sleep(0.62)
+    d = datetime.now()
+    hour = int(datetime.now().strftime("%#I"))
+    drawClock(hour, d.minute, d.second)
--- clock.py
+++ clock.py
@@ -1,64 +1,49 @@
 # clock module
 import turtle as t
-# time data
-hours = 8
-minutes = 55
-seconds = 12
+
+c_radius = 300
 
 #print the time
-def printTime():
+def printTime(hours, minutes, seconds):
     print( f"{hours:02}:{minutes:02}:{seconds:02}" )
 
-# draw clock with turtle
-def drawClock():
+def drawClock(hours, minutes, seconds):
+    printTime(hours, minutes, seconds)
+    t.clear()
+    drawClockCircle()
+    drawClockHours(hours)
+    drawClockMinutes(minutes)
+    drawClockSeconds(seconds)
+
+def drawClockCircle():
+    # the circle
+    t.speed(0)
+    t.penup()
+    t.setheading(0)
+    t.setposition(0, -c_radius)
+    t.pendown()
+    t.circle(c_radius)
+
+def drawClockHours(hours):
+    t.penup()
+    t.setposition(0,0)
     t.pensize(5)
-    t.color("blue")
-    t.circle(100)
-    t.left(90)
-    t.penup()
-    t.forward(100)
-    
-    # clock center dot
-    t.color("red")
-    t.forward(4)
-    t.left(90)
     t.pendown()
-    t.circle(2)
-    
-    # reset the pen
+    t.setheading(90 + hours * -30)
+    t.forward(c_radius * 0.7)
+
+def drawClockMinutes(minutes):
     t.penup()
-    t.right(90)
-    
-    # hours indicator
-    t.color("black")
-    t.pendown()
-    t.right(hours * 30)
-    t.forward(65)
-    
-    # reset the pen
-    t.penup()
-    t.left(180)
-    t.forward(65)
-    t.right(180-hours * 30)
-    
-    # minutes indicator
-    t.color("green")
+    t.setposition(0, 0)
     t.pensize(2)
     t.pendown()
-    t.right(minutes * 6)
-    t.forward(80)
+    t.setheading(90 + minutes * -6)
+    t.forward(c_radius * 0.8)
 
-    # reset the pen
-    t.pensize(1)
+def drawClockSeconds(seconds):
     t.penup()
-    t.left(180)
-    t.forward(80)
-    t.right(180-minutes * 6)
-  
-    # seconds indicator
-    t.color("red")
+    t.setposition(0, 0)
+    t.pensize(1)
     t.pendown()
-    t.right(seconds * 6)
-    t.forward(90)
-    
-    t.mainloop()
+    t.setheading(90 + seconds * -6)
+    t.forward(c_radius * 0.9)
